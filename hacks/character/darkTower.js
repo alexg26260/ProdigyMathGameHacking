74cb2ef9d8b2deb55400d44f8cbc5e4dcac66e36
alexg26260 <77512929+alexg26260@users.noreply.github.com>
2021-05-12 13:03:34 -0400

    Update darkTower.js

--- hacks/character/darkTower.js
+++ hacks/character/darkTower.js
@@ -1,3 +1,4 @@
 //> Dark Tower hack
 //>> Makes your Dark Tower floor to 100, you can add any number, just delete 100 and replace it with the number of you choice if you want!
-_.player.data.tower = 100;
+_.player.data.tower = 1
+;
