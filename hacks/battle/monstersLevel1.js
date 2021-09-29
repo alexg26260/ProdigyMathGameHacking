5b55f82de2bdf166775d69652c2d3c83a577510b
alexg26260 <77512929+alexg26260@users.noreply.github.com>
2021-09-29 15:24:57 -0400

    Update monstersLevel1.js

--- hacks/battle/monstersLevel1.js
+++ hacks/battle/monstersLevel1.js
@@ -1,3 +1,3 @@
-//> Level 1 monster hack
-//>> Makes all the monsters that you're battling level 1 
-_.instance.prodigy.battle.constructor.MOD_DEFAULTS.level = 1;
+//> Level 2 monster hack
+//>> Makes all the monsters that you're battling level 2 
+_.instance.prodigy.battle.constructor.MOD_DEFAULTS.level = 2;
